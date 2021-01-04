12b1d2067c1550b569db596d2b42b4b958384819
andre <andre_epr@outlook.com>
2021-01-03 22:18:13 -0300

    validation

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,25 +1,54 @@
 import React from 'react'
 import { Container } from './style'
 import { useForm, Controller  } from 'react-hook-form'
+import { ErrorMessage } from '@hookform/error-message';
 
 export const Form = () => {
 
-  const { register, handleSubmit, watch, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register, handleSubmit, watch, errors } = useForm({
+    criteriaMode: "all"
+  });
 
+  const onSubmit = data => console.log(data);
+  console.log(watch("example"));
+  console.log(errors)
+  
   return (
+    <>
     <Container>
+
       <form onSubmit={handleSubmit(onSubmit)}>
+
       <label>Example</label>
       <input name="example" defaultValue="test" ref={register} />
       <label>ExampleRequired</label>
       <input
         name="exampleRequired"
-        ref={register({ required: true, maxLength: 10 })}
+        ref={register({
+          required: "This is required.",
+          pattern: {
+            value: /d+/,
+            message: "This input is number only."
+          },
+          maxLength: {
+            value: 10,
+            message: "This input exceed maxLength."
+          }
+        })}
       />
-      {errors.exampleRequired && <p>This field is required</p>}
+      <ErrorMessage errors={errors} name="exampleRequired">
+        {({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      </ErrorMessage>
+      
+
       <input type="submit" />
     </form>
     </Container>
+    </>
   );
 }
