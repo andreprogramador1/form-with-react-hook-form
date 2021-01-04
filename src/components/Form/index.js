eaf5a71c4b9ca6e92196611ee7f8185df7acb88e
andre <andre_epr@outlook.com>
2021-01-03 23:31:33 -0300

    create user done

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,42 +1,158 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container } from './style'
 import { useForm, Controller  } from 'react-hook-form'
 import { ErrorMessage } from '@hookform/error-message';
+import api from '../../services/api' 
 
 export const Form = () => {
 
   const { register, handleSubmit, watch, errors } = useForm({
     criteriaMode: "all"
   });
+  const [data, setData] = useState([]);
 
-  const onSubmit = data => console.log(data);
-  console.log(watch("example"));
-  console.log(errors)
-  
+  useEffect(() => {
+    async function getContent() {
+      const response = await api.get('/users')
+      console.log(response.data)
+      setData(response.data)
+    }
+    getContent()
+  },[])
+
+  function onSubmit(data) {
+    console.log(data)
+    const url = `http://localhost:3333/users/`
+    api.post(url, data)
+    .then((response) => {
+      console.log(response)
+    })
+   
+  }
+
+  // const onSubmit = data => console.log(data);
+
   return (
     <>
     <Container>
 
       <form onSubmit={handleSubmit(onSubmit)}>
 
-      <label>Example</label>
-      <input name="example" defaultValue="test" ref={register} />
-      <label>ExampleRequired</label>
-      <input
-        name="exampleRequired"
+      <label>Name</label>
+      <input name="name"  
         ref={register({
           required: "This is required.",
-          pattern: {
-            value: /d+/,
-            message: "This input is number only."
-          },
           maxLength: {
             value: 10,
             message: "This input exceed maxLength."
+          },
+          minLength: {
+            value: 3,
+            message: "at least 3 characteres"
           }
         })}
       />
-      <ErrorMessage errors={errors} name="exampleRequired">
+
+      <ErrorMessage errors={errors} name="name">
+        {({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      </ErrorMessage>
+
+      <label>Last Name</label>
+      <input
+        name="lastName"
+        ref={register({
+          required: "This is required.",
+          maxLength: {
+            value: 10,
+            message: "This input exceed maxLength."
+          },
+          minLength: {
+            value: 3,
+            message: "at least 3 characteres"
+          }
+        })}
+      />
+      <ErrorMessage errors={errors} name="lastName">
+        {({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      </ErrorMessage>
+
+
+      <label>Email</label>
+      <input
+        name="email"
+        ref={register({
+          required: "This is required.",
+        })}
+      />
+      <ErrorMessage errors={errors} name="email">
+        {({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      </ErrorMessage>
+
+
+      <label>Password</label>
+      <input
+        name="password"
+        ref={register({
+          required: "This is required.",
+          pattern: {
+            value: /^\d+$/,
+            message: "This input is number only."
+          },
+          maxLength: {
+            value: 12,
+            message: "This input exceed maxLength."
+          },
+          minLength: {
+            value: 5,
+            message: "at least 5 characteres"
+          }
+        })}
+      />
+      <ErrorMessage errors={errors} name="password">
+        {({ messages }) =>
+          messages &&
+          Object.entries(messages).map(([type, message]) => (
+            <p key={type}>{message}</p>
+          ))
+        }
+      </ErrorMessage>
+
+
+      <label>Phone</label>
+      <input
+        name="phone"
+        ref={register({
+          required: "This is required.",
+          pattern: {
+            value: /^\d+$/,
+            message: "This input is number only."
+          },
+          maxLength: {
+            value: 12,
+            message: "This input exceed maxLength."
+          },
+          minLength: {
+            value: 5,
+            message: "at least 5 characteres"
+          }
+        })}
+      />
+      <ErrorMessage errors={errors} name="phone">
         {({ messages }) =>
           messages &&
           Object.entries(messages).map(([type, message]) => (
@@ -46,7 +162,7 @@ export const Form = () => {
       </ErrorMessage>
       
 
-      <input type="submit" />
+      <input type="submit"/>
     </form>
     </Container>
     </>
